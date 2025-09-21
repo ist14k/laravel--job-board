9f53fa1cabb453098764b744025e5adb6ee7a42e
ist14k <istiak_akash@outlook.com>
2025-09-21 11:53:15 +0600

    feat: add console command to check JobListingCategory enum values and labels

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('check:enum', function () {
+    print_r(\App\Enum\JobListingCategory::values());
+    print_r(\App\Enum\JobListingCategory::toObj());
+})->purpose('Check Enum Values and Labels');
